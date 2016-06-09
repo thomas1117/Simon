7c9acc15c727fb90fb2badbb88f4bee0c1315b68
Thomas Shannon <thomasshannon1117@yahoo.com>
2016-06-09 14:05:04 -0400

    removed number representations on screen and added overlay on animate

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ var button1 = document.getElementById('1');
 var button2 = document.getElementById('2');
 var button3 = document.getElementById('3');
 var button4 = document.getElementById('4');
+var overlay = document.getElementById('overlay');
 var play = document.getElementById('play');
-var player1 = document.getElementById('player1Arr');
-var player2 = document.getElementById('player2Arr');
 var player1String = '';
 var simon = '';
+
 
 simonButton(button1,1);
 simonButton(button2,2);
@@ -15,14 +15,11 @@ simonButton(button4,4);
 
 play.addEventListener('click',function(){
 	pushToSimon();
-	updateGame();
-	
 });
 
 function simonButton(button,number) {
 	button.addEventListener('click',function(){
 		player1String += number	
-		updateGame();
 		var simonCut = simon.slice(0,player1String.length)
 		comparePlayerLists(player1String,simonCut)
 	});
@@ -32,15 +29,9 @@ function generateRandomNumber(min,max) {
 	return Math.round(Math.random() * (max-min) + 1)
 }
 
-function updateGame() {
-	player1.innerHTML = player1String;
-	player2.innerHTML = simon;
-}
-
 function pushToSimon() {
 	setTimeout(function(){
 		simon += generateRandomNumber(1,4);
-		player2.innerHTML = simon;
 		iterate(simon,600)
 	},1000)
 }
@@ -48,7 +39,6 @@ function pushToSimon() {
 function comparePlayerLists(player1,simonSlice) {
 	if(player1===simonSlice && player1.length===simon.length) {
 		player1String = '';
-		updateGame();
 		pushToSimon();
 	}
 	else if(player1===simonSlice) {
@@ -62,11 +52,13 @@ function resetGame() {
 	player1String = '';
 	simon = '';
 	pushToSimon();
-	updateGame();
+	
 }
 
 function addClass(ele,cls) {
-	ele.className += " " + cls;	
+	if(ele.className.indexOf(cls)===-1) {
+		ele.className += " " + cls;	
+	}
 }
 
 function removeClass(ele,cls) {
@@ -76,6 +68,7 @@ function removeClass(ele,cls) {
 }
 
 function animate(num) {
+	
 	switch(num){
 		case "1":
 			addClass(button1,'test');
@@ -109,10 +102,17 @@ function iterate(arr,time){
 	function loop() {
 		if(count< total) {
 			setTimeout(loop,time);
-			animate(arr[count])
+			animate(arr[count]);
+			addClass(overlay,'overlay');
 			count++;
+		}
+		if(count===total) {
+			removeClass(overlay,'overlay')
 		}		
 }
 loop()
 
+
 };
+
+
